44329b7b4992a594922d2431c6e7e6f5ac2c97ab
the1riddle <elvisotienomboya@gmail.com>
2024-07-14 19:50:25 +0300

    0x02. i18n

---
+++ 0x02-i18n/0-app.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+Basic Flask app
+"""
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def index():
+	""" display index.html file from the templates folder """
+	return render_template('0-index.html')
+
+
+if __name__ == '__main__':
+	app.run(debug = True)
