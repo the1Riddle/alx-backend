ff441fd2685030917f67a99965bc63841b4a1747
the1riddle <elvisotienomboya@gmail.com>
2024-07-14 20:24:25 +0300

    task 1

---
+++ 0x02-i18n/1-app.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+"""
+Basic Flask app
+"""
+from flask import Flask, render_template
+from flask_babel import Babel
+
+
+app = Flask(__name__)
+babel = Babel(app)
+
+
+class Config:
+    """
+    Configuring available languages
+    """
+
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app.config.from_object(Config)
+
+
+@app.route("/", methods=["GET"], strict_slashes=False)
+def index():
+    """
+    display index.html file from the templates folder
+    and has Welcome to Holberton title
+    """
+    return render_template("1-index.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True, host="127.0.0.1", port="5000")
